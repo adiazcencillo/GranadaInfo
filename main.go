9b172e4da8bfd8c50e51978f73e681d54d927843
Jorge <jorgelombardo@correo.ugr.es>
2024-10-17 16:16:44 +0200

    Soluciona #7: se añaden los días festivos así como añade la posibilidad de tener varios horarios el mismo día para los monumentos que abren de mañana y de día

--- main.go
+++ main.go
@@ -1,59 +1,29 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "strings"
+)
 
-// Definir la estructura Ubicacion
 type Ubicacion struct {
-    Calle     string
-    Numero    string
-    Ciudad    string
-    Provincia string
-    Pais      string
+    Calle  string
+    Numero string
 }
 
-// Función para inicializar una instancia de Ubicacion
-func NuevaUbicacion(calle, numero string) Ubicacion {
-    return Ubicacion{
-        Calle:     calle,
-        Numero:    numero,
-        Ciudad:    "Granada",
-        Provincia: "Granada",
-        Pais:      "España",
-    }
-}
-
-// Definir la estructura Horario
 type Horario struct {
-    HorariosApertura map[string]string // Mapa de días de la semana con sus horarios
-    DiasCerrado      [7]bool           // Vector de 7 elementos para los días de la semana
+    DiasCerrado     []bool
+    HorariosApertura map[string][]string
 }
 
-// Función para inicializar una instancia de Horario
-func NuevoHorario(horariosApertura map[string]string, diasCerrado [7]bool) Horario {
-    return Horario{
-        HorariosApertura: horariosApertura,
-        DiasCerrado:      diasCerrado,
-    }
-}
-
-// Definir la estructura Monumento
 type Monumento struct {
     Nombre         string
     Ubicacion      Ubicacion
-    HorarioVerano  *Horario
-    HorarioInvierno *Horario
+    HorarioVerano  Horario
+    HorarioInvierno Horario
     SiempreAbierto bool
 }
 
-// Función para inicializar una instancia de Monumento
-func NuevoMonumento(nombre string, ubicacion Ubicacion, horarioVerano, horarioInvierno *Horario, siempreAbierto bool) Monumento {
-    if siempreAbierto {
-        return Monumento{
-            Nombre:         nombre,
-            Ubicacion:      ubicacion,
-            SiempreAbierto: siempreAbierto,
-        }
-    }
+func NuevoMonumento(nombre string, ubicacion Ubicacion, horarioVerano, horarioInvierno Horario, siempreAbierto bool) Monumento {
     return Monumento{
         Nombre:         nombre,
         Ubicacion:      ubicacion,
@@ -65,74 +35,79 @@ func NuevoMonumento(nombre string, ubicacion Ubicacion, horarioVerano, horarioIn
 
 // Función para mostrar la información del monumento
 func (m Monumento) MostrarInfo() {
-    fmt.Printf("Nombre: %s\nUbicación: %s, %s, %s, %s, %s\n",
-        m.Nombre, m.Ubicacion.Calle, m.Ubicacion.Numero, m.Ubicacion.Ciudad,
-        m.Ubicacion.Provincia, m.Ubicacion.Pais)
+    fmt.Printf("Nombre: %s\nUbicación: %s, %s\n",
+        m.Nombre, m.Ubicacion.Calle, m.Ubicacion.Numero)
     
     if m.SiempreAbierto {
         fmt.Println("Este monumento está siempre abierto.")
         return
     }
 
+    diasSemana := []string{"Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo", "Festivo"}
+
     fmt.Printf("Horario de Verano:\n")
-    for dia, horario := range m.HorarioVerano.HorariosApertura {
-        fmt.Printf("  %s: %s\n", dia, horario)
-    }
-    diasSemana := []string{"Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"}
-    fmt.Printf("Días Cerrado en Verano:\n")
-    for i, cerrado := range m.HorarioVerano.DiasCerrado {
+    for i, dia := range diasSemana {
         estado := "Abierto"
-        if cerrado {
+        if m.HorarioVerano.DiasCerrado[i] {
             estado = "Cerrado"
+            fmt.Printf("  %s: %s\n", dia, estado)
+        } else {
+            horarios := m.HorarioVerano.HorariosApertura[dia]
+            fmt.Printf("  %s: %s - %s\n", dia, estado, strings.Join(horarios, ", "))
         }
-        fmt.Printf("  %s: %s\n", diasSemana[i], estado)
     }
+
     fmt.Printf("Horario de Invierno:\n")
-    for dia, horario := range m.HorarioInvierno.HorariosApertura {
-        fmt.Printf("  %s: %s\n", dia, horario)
-    }
-    fmt.Printf("Días Cerrado en Invierno:\n")
-    for i, cerrado := range m.HorarioInvierno.DiasCerrado {
+    for i, dia := range diasSemana {
         estado := "Abierto"
-        if cerrado {
+        if m.HorarioInvierno.DiasCerrado[i] {
             estado = "Cerrado"
+            fmt.Printf("  %s: %s\n", dia, estado)
+        } else {
+            horarios := m.HorarioInvierno.HorariosApertura[dia]
+            fmt.Printf("  %s: %s - %s\n", dia, estado, strings.Join(horarios, ", "))
         }
-        fmt.Printf("  %s: %s\n", diasSemana[i], estado)
     }
 }
 
 func main() {
     // Crear una instancia de Ubicacion
-    ubicacionAlhambra := NuevaUbicacion("Calle Real de la Alhambra", "s/n")
-
-    // Crear una instancia de Horario de Verano
-    horariosAperturaVerano := map[string]string{
-        "Lunes":    "08:30 - 14:00",
-        "Martes":   "08:30 - 14:00",
-        "Miércoles": "08:30 - 14:00",
-        "Jueves":   "08:30 - 14:00",
-        "Viernes":  "08:30 - 14:00",
-        "Sábado":   "08:30 - 14:00",
-        "Domingo":  "08:30 - 14:00",
+    ubicacionAlhambra := Ubicacion{
+        Calle:  "Calle Real de la Alhambra",
+        Numero: "s/n",
     }
-    diasCerradoVerano := [7]bool{false, false, false, false, false, false, true} // Cerrado los domingos en verano
-    horarioVerano := NuevoHorario(horariosAperturaVerano, diasCerradoVerano)
 
-    // Crear una instancia de Horario de Invierno
-    horariosAperturaInvierno := map[string]string{
-        "Lunes":    "09:00 - 13:00",
-        "Martes":   "09:00 - 13:00",
-        "Miércoles": "09:00 - 13:00",
-        "Jueves":   "09:00 - 13:00",
-        "Viernes":  "09:00 - 13:00",
-        "Sábado":   "09:00 - 13:00",
-        "Domingo":  "09:00 - 13:00",
+    // Crear horarios de ejemplo
+    horarioVerano := Horario{
+        DiasCerrado: []bool{false, false, false, false, false, false, false, false},
+        HorariosApertura: map[string][]string{
+            "Lunes":    {"09:00-13:00", "15:00-18:00"},
+            "Martes":   {"09:00-13:00", "15:00-18:00"},
+            "Miércoles": {"09:00-13:00", "15:00-18:00"},
+            "Jueves":   {"09:00-13:00", "15:00-18:00"},
+            "Viernes":  {"09:00-13:00", "15:00-18:00"},
+            "Sábado":   {"09:00-13:00", "15:00-18:00"},
+            "Domingo":  {"09:00-13:00", "15:00-18:00"},
+            "Festivo":  {"09:00-13:00", "15:00-18:00"},
+        },
     }
-    diasCerradoInvierno := [7]bool{false, false, false, false, false, true, true} // Cerrado los sábados y domingos en invierno
-    horarioInvierno := NuevoHorario(horariosAperturaInvierno, diasCerradoInvierno)
+
+    horarioInvierno := Horario{
+        DiasCerrado: []bool{false, false, false, false, false, false, false, false},
+        HorariosApertura: map[string][]string{
+            "Lunes":    {"10:00-14:00", "16:00-19:00"},
+            "Martes":   {"10:00-14:00", "16:00-19:00"},
+            "Miércoles": {"10:00-14:00", "16:00-19:00"},
+            "Jueves":   {"10:00-14:00", "16:00-19:00"},
+            "Viernes":  {"10:00-14:00", "16:00-19:00"},
+            "Sábado":   {"10:00-14:00", "16:00-19:00"},
+            "Domingo":  {"10:00-14:00", "16:00-19:00"},
+            "Festivo":  {"10:00-14:00", "16:00-19:00"},
+        },
+    }
 
     // Crear una instancia de Monumento
-    alhambra := NuevoMonumento("La Alhambra", ubicacionAlhambra, &horarioVerano, &horarioInvierno, false)
+    alhambra := NuevoMonumento("La Alhambra", ubicacionAlhambra, horarioVerano, horarioInvierno, false)
 
     // Mostrar la información del monumento
     alhambra.MostrarInfo()
