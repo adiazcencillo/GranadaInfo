bb088dbe559891aa2677f482846e8ff1e565e5a2
Jorge <jorgelombardo@correo.ugr.es>
2024-10-24 12:41:39 +0200

    closes #7 and closes #8

--- monumento/ubicacion.go
+++
@@ -1,8 +0,0 @@
-package monumento
-
-// Ubicacion representa la dirección de un monumento. No se añade cuidad ni país ya que todos están en Granada.
-// Una ubicación no tiene identidad propia, basa su identidad en sus atributos, por tanto es un objeto valor
-type Ubicacion struct {
-    Calle  string  // Nombre de la calle
-    Numero string  // Número de la calle
-}
