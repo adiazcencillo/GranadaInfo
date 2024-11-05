6ca098bc707c871d626a80b25afbe2d70a7c61e9
Jorge <jorgelombardo@correo.ugr.es>
2024-11-05 16:53:07 +0100

    closes #17 espceificando qué es la entidad Monumento

--- pkg/horario/monumento.go
+++ pkg/horario/monumento.go
@@ -1,15 +1,10 @@
 package horario
 
-// Monumento representa un monumento con su información básica.
-// Un Monumento tiene atributos que describen su identidad inmutable.
-// Por ejemplo, el atributo Nombre garantiza la unicidad del monumento, considerándose así una entidad.
 type Monumento struct {
-	Nombre         string  // Nombre del monumento.
-	Horario        Horario // Horario de apertura y cierre del monumento.
+	Nombre         string
+	Horario        Horario
 }
 
-// NuevoMonumento crea y devuelve una nueva instancia de Monumento.
-// Recibe el nombre del monumento y su horario
 func NuevoMonumento(nombre string, horario Horario) Monumento {
 	return Monumento{
 		Nombre:         nombre,
