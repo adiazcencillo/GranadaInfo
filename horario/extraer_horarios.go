d3d2ebeaecd23e7bdf47dd6a0b05226b5eb602f0
adiazcencillo <albertodiazcen@gmail.com>
2024-12-17 20:42:28 +0100

    closes #31
    Añado código para la función cargarDocumento de forma que se ajuste a los requerimientos especificados en su test

---
+++ horario/extraer_horarios.go
@@ -0,0 +1,26 @@
+package horario
+
+import(
+	"fmt"
+	"os"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+func cargarDocumento(filePath string) (*html.Node, error) {
+
+	archivo, err := os.Open(filePath)
+	if err != nil {
+		return nil, fmt.Errorf("No se pudo abrir el archivo: %w", err)
+	}
+	defer archivo.Close()
+
+	contenido, err := html.Parse(archivo)
+	if err != nil {
+		return nil, fmt.Errorf("Error parseando el HTML: %w", err)
+	}
+
+	return contenido, nil
+
+}
