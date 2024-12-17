80707cac20ab1cf33502fac2c56c5667850e2f76
adiazcencillo <albertodiazcen@gmail.com>
2024-12-17 20:50:51 +0100

    closes #31
    Añado código para la función extraerNodosH3 de forma que se ajuste a los requerimientos especificados en su test

--- horario/extraer_horarios.go
+++ horario/extraer_horarios.go
@@ -24,3 +24,26 @@ func cargarDocumento(filePath string) (*html.Node, error) {
 	return contenido, nil
 
 }
+
+func extraerNodosH3(doc *html.Node) ([]*html.Node, error) {
+	var nodosH3 []*html.Node
+
+	var recorrerNodos func(*html.Node)
+	recorrerNodos = func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "h3" {
+			texto := extraerStringNodo(n)
+
+			if texto != "MONUMENTOS DE GRANADA" {
+				nodosH3 = append(nodosH3, n)
+			}
+		}
+
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			recorrerNodos(c)
+		}
+	}
+
+	recorrerNodos(doc)
+
+	return nodosH3, nil
+}
