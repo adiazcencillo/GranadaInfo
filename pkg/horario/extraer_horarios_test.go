2c49d108b86ae4e3b60feb2932d51c1a2b06f7e2
adiazcencillo <albertodiazcen@gmail.com>
2024-12-17 17:15:12 +0100

    closes #25
    Se añaden dos funciones más de test: una para comprobar la correcta extracción de los nodos donde se encuentra la información de horarios, y otra para comprobar que se extrae correctamente la información de los nodos obtenidos. De esta forma podemos comenzar a trabajar sobre la infromación

--- pkg/horario/extraer_horarios_test.go
+++ pkg/horario/extraer_horarios_test.go
@@ -25,14 +25,45 @@ func TestCargarDocumento(t *testing.T) {
 func TestExtraerNodosH3(t *testing.T) {
 	g := gomega.NewWithT(t)
 
-	filePath := "test_single_block.html"
+	filePath := "~/horarios.html"
 	doc, err := cargarDocumento(filePath)
 	g.Expect(err).NotTo(gomega.HaveOccurred())
 
 	nodosH3, err := extraerNodosH3(doc)
 	g.Expect(err).NotTo(gomega.HaveOccurred())
 	g.Expect(nodosH3).To(gomega.HaveLen(21))
+	
 }
 
+func TestExtraerNodosHorarios(t *testing.T) {
+	g := gomega.NewWithT(t)
 
+	filePath := "~/horarios.html"
+	doc, err := cargarDocumento(filePath)
+	g.Expect(err).NotTo(gomega.HaveOccurred())
+
+	nodosHorario, err := extraerNodosHorario(doc)
+	g.Expect(err).NotTo(gomega.HaveOccurred())
+	g.Expect(nodosH3).To(gomega.HaveLen(21))
+}	
+
+func TestExtraerStringNodo(t *testing.T) {
+	g := gomega.NewWithT(t)
+
+	filePath := "~/horarios.html"
+	doc, err := cargarDocumento(filePath)
+	g.Expect(err).NotTo(gomega.HaveOccurred())
+
+	nodosH3 := extraerNodosH3(doc)
+	nodosHorario := extraerNodosHorario(doc)
+
+	texto, err := extraerStringNodo(nodosH3[0])
+	g.Expect(err).NotTo(gomega.HaveOccurred())
+	g.Expect(texto).To(gomega.Equal("Alhambra"))
+
+	texto, err := extraerStringNodo(nodosHorario[0])
+	g.Expect(err).NotTo(gomega.HaveOccurred())
+	g.Expect(texto).To(gomega.Equal("08:30 - 18:00, 19:00 - 20:45 (invierno); 08:30 - 20:00, 22:00 - 23:30 (verano)"))
+
+}
 
