cb63367a9cf02167a656d9cb8823b9edc59a0c58
adiazcencillo <albertodiazcen@gmail.com>
2024-12-17 13:48:55 +0100

    closes #25
    Mediante la implementación de este test nos aseguramos de que podemos cargar de forma correcta la información de horarios y monumentos presente en el archivo horarios.html. Una vez la información está cargada de forma correcta, podemos comenzar a extraerla

---
+++ pkg/horario/extraer_horarios_test.go
@@ -0,0 +1,22 @@
+package horario
+
+import(
+	"testing"
+
+	"golang.org/x/net/html"
+
+	"github.com/onsi/gomega"
+)
+
+func TestCargarDocumento(t *testing.T) {
+	g := gomega.NewWithT(t)
+
+	t.Run("Cargar documento HTML correctamente", func(t *testing.T) {
+		filePath := "~/horarios.html"
+
+		doc, err := cargarDocumento(filePath)
+
+		g.Expect(err).NotTo(gomega.HaveOccurred()) 
+		g.Expect(doc).NotTo(gomega.BeNil())         
+	})
+}
