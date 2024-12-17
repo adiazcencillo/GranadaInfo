695bb4e945eb656e2a2117daf44492dbac4ee2a5
adiazcencillo <albertodiazcen@gmail.com>
2024-12-17 16:57:41 +0100

    closes #25
    Continuando con la extracción de información, queremos asegurarnos que podemos recuperar los nodos html donde se encuentran los nombres de los monumentos de forma satisfactoria. Queremos recuperar tantos nodos como monumentos vamos a extraer (21)

--- pkg/horario/extraer_horarios_test.go
+++ pkg/horario/extraer_horarios_test.go
@@ -12,7 +12,7 @@ func TestCargarDocumento(t *testing.T) {
 	g := gomega.NewWithT(t)
 
 	t.Run("Cargar documento HTML correctamente", func(t *testing.T) {
-		
+
 		filePath := "~/horarios.html"
 
 		doc, err := cargarDocumento(filePath)
@@ -21,4 +21,18 @@ func TestCargarDocumento(t *testing.T) {
 		g.Expect(doc).NotTo(gomega.BeNil())         
 	})
 }
+
+func TestExtraerNodosH3(t *testing.T) {
+	g := gomega.NewWithT(t)
+
+	filePath := "test_single_block.html"
+	doc, err := cargarDocumento(filePath)
+	g.Expect(err).NotTo(gomega.HaveOccurred())
+
+	nodosH3, err := extraerNodosH3(doc)
+	g.Expect(err).NotTo(gomega.HaveOccurred())
+	g.Expect(nodosH3).To(gomega.HaveLen(21))
+}
+
+
 
