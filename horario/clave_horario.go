1f5ee5aeec527f6c1646c11dddabec9e664cc869
adiazcencillo <albertodiazcen@gmail.com>
2024-12-20 18:10:30 +0100

    closes #15
    Se modifica la definición de clave horario para poder simplificar la funcion extraerHorarios

--- horario/clave_horario.go
+++ horario/clave_horario.go
@@ -3,12 +3,12 @@ package horario
 type ClaveHorario string
 
 const (
-    Lunes     ClaveHorario = "Lunes"
-    Martes    ClaveHorario = "Martes"
-    Miércoles ClaveHorario = "Miércoles"
-    Jueves    ClaveHorario = "Jueves"
-    Viernes   ClaveHorario = "Viernes"
-    Sábado    ClaveHorario = "Sábado"
-    Domingo   ClaveHorario = "Domingo"
-    Festivo   ClaveHorario = "Festivo"
+    Lunes     ClaveHorario = "lunes"
+    Martes    ClaveHorario = "martes"
+    Miércoles ClaveHorario = "miércoles"
+    Jueves    ClaveHorario = "jueves"
+    Viernes   ClaveHorario = "viernes"
+    Sábado    ClaveHorario = "sábado"
+    Domingo   ClaveHorario = "domingo"
+    Festivo   ClaveHorario = "festivo"
 )
